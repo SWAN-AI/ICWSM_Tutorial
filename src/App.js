66ea6ea290e53484d3dc33ccab33f668ac52ab8c
Agnik Saha <agniksaha2019@gmail.com>
2026-03-16 09:50:19 -0400

    Fix broken React files for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-// UseQuery
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-// Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import "./App.css";
-
-// Components
 import HomePage from "./components/Pages/HomePage";
-
-// Router
 import { Route, Routes } from "react-router-dom";
 
 const queryClient = new QueryClient();
@@ -17,13 +9,9 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="App">
-        <div>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-          </Routes>
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </QueryClientProvider>
   );
 }
--- src/components/Pages/HomePage.js
+++ src/components/Pages/HomePage.js
@@ -1,13 +1,7 @@
-import { Container } from "reactstrap";
-
-// Components
 import React from "react";
-import Panner from "../Panner/Panner";
 import MyNavbar from "../MyNavbar/MyNavbar";
 import HeroSection from "../HeroSection/HeroSection";
-import BrandsSection from "../BrandsSection/BrandsSection";
 import BenefitsSection from "../BenefitsSection/BenefitsSection";
-import CoursesSection from "../CoursesSection/CoursesSection";
 import TestimonialsSection from "../TestimonialsSection/TestimonialsSection";
 import FAQSection from "../FAQSection/FAQSection";
 import Footer from "../Footer/Footer";
@@ -16,14 +10,10 @@ const HomePage = () => {
   return (
     <>
       <MyNavbar />
-      <Container className="mb-5">
-        <HeroSection />
-        {/* <BrandsSection /> */}
-        <BenefitsSection />
-        {/* <CoursesSection /> */}
-        <TestimonialsSection />
-        <FAQSection />
-      </Container>
+      <HeroSection />
+      <BenefitsSection />
+      <TestimonialsSection />
+      <FAQSection />
       <Footer />
     </>
   );
